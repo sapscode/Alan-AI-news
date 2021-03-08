8aceaf9adee34186200a5197418d56b1846e931d
Saptarshi Hore <saptarshi0009@gmail.com>
2021-03-08 18:50:40 +0530

    alan ai basic setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import { useEffect } from 'react';
 
-function App() {
-  return (
+const alanKey = '40d52f6504cf363d9a2100998d2e62522e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command }) => {
+                if(command === 'testCommand'){
+                    alert('This code is executed')
+                }
+            }
+        });
+    }, []);
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Alan AI News</h1>
     </div>
-  );
+    );
 }
 
 export default App;
