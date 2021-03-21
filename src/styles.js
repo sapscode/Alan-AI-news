a6136213078f13affbf13a1ad0f646d6d0414132
Saptarshi Hore <saptarshi0009@gmail.com>
2021-03-21 12:35:26 +0530

    footer code removed from styles

--- src/styles.js
+++ src/styles.js
@@ -1,21 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-  footer: {
-    textAlign: 'center',
-    position: 'fixed',
-    left: 0,
-    bottom: 0,
-    color: 'black',
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '120px',
-    [theme.breakpoints.down('sm')]: {
-      display: 'none',
-    },
-  },
   link: {
     textDecoration: 'none',
     color: 'rgba(21, 101, 192)',
